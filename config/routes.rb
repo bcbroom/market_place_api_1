7ee2290754567e4cf8c20b1d4853f8a5c578c46f
Brian Broom <brian.broom@gmail.com>
2020-04-06 11:37:25 -0400

    Finishes modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
       get 'tokens/create'
     end
   end
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
+      resources :products, only: [:show, :index]
     end
   end
+
 end
